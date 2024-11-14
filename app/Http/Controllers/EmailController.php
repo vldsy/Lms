175a9304baae06958a2223a07f5c417d59cd245e
XoDeR <vldsyata@gmail.com>
2024-11-14 21:50:13 +0100

    added notes

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Mail\SubmissionSent;
 use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 class EmailController extends Controller
 {
     public function sendEmail(Request $request)
     {
-        $data['name'] = 'John';
+        $randomUser = User::inRandomOrder()->first();
+        $data['name'] = $randomUser->name;
         Mail::to('recipient@example.com')->send(new SubmissionSent($data));
         return response()->json(['message' => 'Email sent']);
     }
